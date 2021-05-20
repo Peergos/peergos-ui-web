09438deef0f14165145080501898eb5520dd7ca3
kevin odwyer <kevodwyer@gmail.com>
2021-05-20 12:29:47 +0100

    change bridge to email-bridge

--- src/components/filesystem/index.js
+++ src/components/filesystem/index.js
@@ -538,7 +538,7 @@ module.exports = {
 
 		    var pendingOutgoingUsernames = [];
 		    socialState.pendingOutgoing.toArray([]).map(u => pendingOutgoingUsernames.push(u));
-            if(friendNames.includes('bridge')) {
+            if(friendNames.includes('email-bridge')) {
                 that.isEmailAvailable = true;
             }
 		    that.social = {
